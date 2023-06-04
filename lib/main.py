49d4e5b07b1f7db8ef91d30e1c1dcc0c329b8ed7
nimo <nimo.sheikh@student.moringaschool.com>
2023-06-04 19:12:14 +0300

    completed assignmemt

--- lib/main.py
+++ lib/main.py
@@ -3,31 +3,45 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 from pydantic import BaseModel
-# from app.routes import router
+# from app.routes import routerz
+
+
 # Create the FastAPI instance
 app = FastAPI()
+
+
 # Create the database connection
 SQLALCHEMY_DATABASE_URL = "sqlite:///./my_database.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
 # Define the database model
 class MyData(Base):
     __tablename__ = "mydata"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
+
+
 # Create the database tables
 Base.metadata.create_all(bind=engine)
+
+
 # Pydantic model for POST and PUT requests
 class MyDataCreateUpdate(BaseModel):
     name: str
     description: str
+
+
 # Pydantic model for GET responses
 class MyDataResponse(BaseModel):
     id: int
     name: str
     description: str
+
+
 # Function to get a database session
 def get_db():
     db = SessionLocal()
@@ -35,11 +49,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
 # GET all data
 @app.get("/get_all_endpoint/")
 def get_all_data(db: Session = Depends(get_db)):
     data = db.query(MyData).all()
     return data
+
+
 # GET single data
 @app.get("/get_one_endpoint/{id}")
 def get_single_data(id: int, db: Session = Depends(get_db)):
@@ -47,6 +65,8 @@ def get_single_data(id: int, db: Session = Depends(get_db)):
     if data is None:
         raise HTTPException(status_code=404, detail="Data not found")
     return data
+
+
 # POST new data
 @app.post("/post_endpoint/")
 def create_data(item: MyDataCreateUpdate, db: Session = Depends(get_db)):
@@ -55,6 +75,8 @@ def create_data(item: MyDataCreateUpdate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(data)
     return data
+
+
 # PUT full update of data
 @app.put("/put_endpoint/{id}")
 def update_data(id: int, item: MyDataCreateUpdate, db: Session = Depends(get_db)):
@@ -66,6 +88,8 @@ def update_data(id: int, item: MyDataCreateUpdate, db: Session = Depends(get_db)
     db.commit()
     db.refresh(data)
     return data
+
+
 # PATCH partial update of data
 @app.patch("/patch_endpoint/{id}")
 def partial_update_data(id: int, item: MyDataCreateUpdate, db: Session = Depends(get_db)):
@@ -79,6 +103,8 @@ def partial_update_data(id: int, item: MyDataCreateUpdate, db: Session = Depends
     db.commit()
     db.refresh(data)
     return data
+
+
 # DELETE a piece of data
 @app.delete("/delete_endpoint/{id}")
 def delete_data(id: int, db: Session = Depends(get_db)):
